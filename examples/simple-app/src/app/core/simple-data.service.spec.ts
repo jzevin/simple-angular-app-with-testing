aac372add689d2eae535c0c51c19c0f567c58aac
jzevin <jeremy.zevin@gmail.com>
2024-07-08 11:33:40 -0400

    chore: Add SimpleDataService and HttpClient providers to app component spec

--- examples/simple-app/src/app/core/simple-data.service.spec.ts
+++ examples/simple-app/src/app/core/simple-data.service.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SimpleDataService } from './simple-data.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('SimpleDataService', () => {
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
   let service: SimpleDataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SimpleDataService);
+    TestBed.configureTestingModule({
+      imports: [],
+      providers: [SimpleDataService]
+    });
+    service =  new SimpleDataService(httpClientSpy);
   });
 
   it('should be created', () => {
