743da71bc4ddf142776bc72c983e54f4d1e5bde1
jzevin <jeremy.zevin@gmail.com>
2024-07-08 14:11:40 -0400

    refactor: app component spec to use mocked services

--- examples/simple-app/src/app/app.component.spec.ts
+++ examples/simple-app/src/app/app.component.spec.ts
@@ -1,20 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { SimpleDataService } from './core/simple-data.service';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
-
   let fixture: ComponentFixture<AppComponent>;
   let app: AppComponent;
-  
+  let simpleDataService: jasmine.SpyObj<SimpleDataService>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [SimpleDataService, provideHttpClient()]
+      providers: [
+        {
+          provide: SimpleDataService,
+          useValue: jasmine.createSpyObj('SimpleDataService', { getName: of('foo') }),
+          // Mock implementation returns an observable
+        },
+        {
+          provide: HttpClient,
+          useValue: jasmine.createSpyObj('HttpClient', ['get']),
+        },
+      ],
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.componentInstance;
+    simpleDataService = TestBed.inject(SimpleDataService) as jasmine.SpyObj<SimpleDataService>;
   });
 
   it('should create the app', () => {
