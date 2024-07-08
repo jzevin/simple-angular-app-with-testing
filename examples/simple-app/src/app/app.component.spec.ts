c310b09ffa72287c718d274809c050f4c0bb02af
jzevin <jeremy.zevin@gmail.com>
2024-07-08 13:49:16 -0400

    refactor: app component spec to use ComponentFixture for testing

--- examples/simple-app/src/app/app.component.spec.ts
+++ examples/simple-app/src/app/app.component.spec.ts
@@ -1,19 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { SimpleDataService } from './core/simple-data.service';
 import { provideHttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [SimpleDataService, provideHttpClient()]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 });
