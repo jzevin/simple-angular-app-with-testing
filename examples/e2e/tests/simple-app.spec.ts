6b98855e5c26aa8044ff9b3d85a12c6d06dea073
jzevin <jeremy.zevin@gmail.com>
2024-07-09 11:44:35 -0400

    feat: Add a failing e2e test for checking the visibility and initial classes of the face icon

--- examples/e2e/tests/simple-app.spec.ts
+++ examples/e2e/tests/simple-app.spec.ts
@@ -7,4 +7,14 @@ test.describe('Simple App', () => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/SimpleApp/);
   });
+
+  test('face icon is visible and has the correct initial classes', async ({ page }) => {
+    await page.goto('http://localhost:4200/');
+
+    // Expect an element "to be visible".
+    await expect(page.locator('i').first()).toBeVisible();
+
+    // Expect an element "to have class".
+    await expect(page.locator('i').first()).toHaveClass('bi-emoji-frown-fill');
+  });
 });
