fd522701ec6d1101f7eee8987e27708db55b31d1
jzevin <jeremy.zevin@gmail.com>
2024-07-09 10:18:39 -0400

    feat: Add e2e test for checking the title of the Simple App

--- examples/e2e/tests/simple-app.spec.ts
+++ examples/e2e/tests/simple-app.spec.ts
@@ -1,5 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Simple App', () => {
-  
+  test('has title', async ({ page }) => {
+    await page.goto('http://localhost:4200/');
+
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/SimpleApp/);
+  });
 });
